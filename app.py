ad523328aa3db5b4faf63aa03d82f05d00417678
Russ Mckendrick <russ@mckendrick.io>
2021-02-02 09:33:58 +0000

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Testing a deploy from DevOps!!!"
+    return "Testing a updating the container!!!"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
