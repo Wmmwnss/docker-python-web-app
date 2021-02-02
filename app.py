75023e5c918d391a259d73aa709a52f40aebbb63
Russ Mckendrick <russ@mckendrick.io>
2021-02-02 16:07:55 +0000

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Testing a updating the container!!!"
+    return "Hello there Robert!!!"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
