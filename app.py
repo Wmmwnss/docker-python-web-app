eaf96f4f14ecdfaba3d9f631f9cc853f25fa5afa
Russ McKendrick <russ@mckendrick.io>
2021-02-04 13:06:22 +0000

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello there Robert!!!"
+    return "Testing an Azure Devops deployment !!!!"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
